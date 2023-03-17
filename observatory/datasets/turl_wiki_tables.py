d22517f4736ed390cda0cf71a6f8026c13d5f6e1
superctj <congtj@umich.edu>
2023-03-16 23:12:17 -0400

    process turl wikitable dataset for obtaining column representations

--- observatory/datasets/turl_wiki_tables.py
+++ observatory/datasets/turl_wiki_tables.py
@@ -163,48 +163,200 @@ def prepare_data_for_lm(data_dir, entity_id_map) -> dict[int, List[List[str]]]:
     return id2metadata
 
 
-def process_single_hybrid_table(input_table, config):
-    headers, entities, entities_text = input_table
+def process_single_table_for_columns(input_table, config):
+    table_id, pg_title, pg_entity, sec_title, caption, headers, entities = input_table
+    pg_entity = -1 # do not take page entity as input
+
+    tokenized_pg_title = config.tokenizer.encode(pg_title, max_length=config.max_title_length, add_special_tokens=False)
+    tokenized_meta = tokenized_pg_title + \
+                    config.tokenizer.encode(sec_title, max_length=config.max_title_length, add_special_tokens=False)
     
-    tokenized_headers = [config.tokenizer.encode(z, max_length=config.max_header_length, add_special_tokens=False) for _, z in headers]
+    if caption != sec_title:
+        tokenized_meta += config.tokenizer.encode(caption, max_length=config.max_title_length, add_special_tokens=False)
+    
+    tokenized_headers = [config.tokenizer.encode(z, max_length=config.max_header_length, add_special_tokens=False) for z in headers]
 
-    input_tokens = []
-    input_tokens_pos = []
-    input_tokens_type = []
-
+    input_tok = []
+    input_tok_pos = []
+    input_tok_type = []
+    tokenized_meta_length = len(tokenized_meta)
+    input_tok += tokenized_meta
+    input_tok_pos += list(range(tokenized_meta_length))
+    input_tok_type += [0] * tokenized_meta_length
     tokenized_headers_length = [len(z) for z in tokenized_headers]
-    input_tokens += list(itertools.chain(*tokenized_headers))
-    input_tokens_pos += list(itertools.chain(*[list(range(z)) for z in tokenized_headers_length]))
-    input_tokens_type += [1] * sum(tokenized_headers_length)
+    input_tok += list(itertools.chain(*tokenized_headers))
+    input_tok_pos += list(itertools.chain(*[list(range(z)) for z in tokenized_headers_length]))
+    input_tok_type += [1] * sum(tokenized_headers_length)
 
-    input_entities = [entity for _, entity in entities]
-    input_entities_text = [config.tokenizer.encode(entity_text, max_length=config.max_cell_length, add_special_tokens=False) if len(entity_text) != 0 else [] for entity_text in entities_text]
+    input_ent = []
+    input_ent_text = []
+    input_ent_type = []
+    column_en_map = {}
+    row_en_map = {}
+
+    # index refers to (row_index, column_index) of an entity in the table
+    for e_i, (index, (entity_id, entity_text)) in enumerate(entities):
+        tokenized_entity_text = config.tokenizer.encode(entity_text, max_length=config.max_cell_length, add_special_tokens=False)
+        
+        input_ent.append(entity_id)
+        input_ent_text.append(tokenized_entity_text)
+        input_ent_type.append(4)
+
+        if index[0] not in row_en_map:
+            row_en_map[index[0]] = [e_i]
+        else:
+            row_en_map[index[0]].append(e_i)
+
+        if index[1] not in column_en_map:
+            column_en_map[index[1]] = [e_i]
+        else:
+            column_en_map[index[1]].append(e_i)
     
-    input_entities_cell_length = [len(x) if len(x) != 0 else 1 for x in input_entities_text]
-    max_cell_length = max(input_entities_cell_length)
-    input_entities_text_padded = np.zeros([len(input_entities_text), max_cell_length], dtype=int)
-    for i, x in enumerate(input_entities_text):
-        input_entities_text_padded[i, :len(x)] = x
+    # create column entity mask
+    column_entity_mask = np.zeros([len(headers), len(input_ent)], dtype=int)
+    for j in range(len(headers)):
+        for e_i_1 in column_en_map[j]:
+            column_entity_mask[j, e_i_1] = 1
 
-    # input_tokens = torch.LongTensor([input_tokens])
-    # input_tokens_type = torch.LongTensor([input_tokens_type])
-    # input_tokens_pos = torch.LongTensor([input_tokens_pos])
+    # create column header mask
+    start_i = 0
+    header_span = {}
+    column_header_mask = np.zeros([len(headers), len(input_tok)], dtype=int)
+    for j in range(len(headers)):
+        header_span[j] = (start_i, start_i+tokenized_headers_length[j])
+        column_header_mask[j, tokenized_meta_length+header_span[j][0]:tokenized_meta_length+header_span[j][1]] = 1
+        start_i += tokenized_headers_length[j]
 
-    # input_entities = torch.LongTensor([input_entities])
-    # input_entities_text = torch.LongTensor([input_entities_text_padded])
-    # input_entities_cell_length = torch.LongTensor([input_entities_cell_length])
-    # input_entities_type = torch.LongTensor([input_entities_type])
+    # create input mask
+    tok_tok_mask = np.ones([len(input_tok), len(input_tok)], dtype=int)
+    meta_ent_mask = np.ones([tokenized_meta_length, len(input_ent)], dtype=int)
+    header_ent_mask = np.zeros([sum(tokenized_headers_length), len(input_ent)], dtype=int)
+    
+    for e_i, (index, _) in enumerate(entities):
+        header_ent_mask[header_span[index[1]][0]:header_span[index[1]][1], e_i] = 1
+    ent_header_mask = np.transpose(header_ent_mask)
 
-    return [np.array(input_tokens), np.array(input_tokens_type), np.array(input_tokens_pos), len(input_tokens), np.array(input_entities), input_entities_text_padded, input_entities_cell_length, len(input_entities)]
+    input_tok_mask = [tok_tok_mask, np.concatenate([meta_ent_mask, header_ent_mask], axis=0)]
+    ent_meta_mask = np.ones([len(input_ent), tokenized_meta_length], dtype=int)
+    
+    ent_ent_mask = np.eye(len(input_ent), dtype=int)
+    for _,e_is in column_en_map.items():
+        for e_i_1 in e_is:
+            for e_i_2 in e_is:
+                ent_ent_mask[e_i_1, e_i_2] = 1
+    for _,e_is in row_en_map.items():
+        for e_i_1 in e_is:
+            for e_i_2 in e_is:
+                ent_ent_mask[e_i_1, e_i_2] = 1
+    input_ent_mask = [np.concatenate([ent_meta_mask, ent_header_mask], axis=1), ent_ent_mask]
+
+    # prepend pgEnt to input_ent, input_ent[0] = pgEnt
+    if pg_entity != -1:
+        input_tok_mask[1] = np.concatenate([np.ones([len(input_tok), 1], dtype=int), input_tok_mask[1]], axis=1)
+    else:
+        input_tok_mask[1] = np.concatenate([np.zeros([len(input_tok), 1], dtype=int), input_tok_mask[1]], axis=1)
+
+    """
+    The following code is commented out so that page entity and page title are not included in entities and entity mentions
+    """
+    # input_ent = [pg_entity if pg_entity != -1 else 0] + input_ent
+    # input_ent_text = [tokenized_pg_title[:config.max_cell_length]] + input_ent_text
+    # input_ent_type = [2] + input_ent_type
+
+    # new_input_ent_mask = [np.ones([len(input_ent), len(input_tok)], dtype=int), np.ones([len(input_ent), len(input_ent)], dtype=int)]
+    # new_input_ent_mask[0][1:, :] = input_ent_mask[0]
+    # new_input_ent_mask[1][1:, 1:] = input_ent_mask[1]
+    # if pg_entity == -1:
+    #     new_input_ent_mask[1][:, 0] = 0
+    #     new_input_ent_mask[1][0, :] = 0
+    # column_entity_mask = np.concatenate([np.zeros([len(headers), 1], dtype=int),column_entity_mask],axis=1)
+
+    # input_ent_mask = new_input_ent_mask
+    # labels = np.zeros([len(type_annotations), config.type_num], dtype=int)
+    # for j, types in enumerate(type_annotations):
+    #     for t in types:
+    #         labels[j, config.type_vocab[t]] = 1
+    input_ent_cell_length = [len(x) if len(x) != 0 else 1 for x in input_ent_text]
+    max_cell_length = max(input_ent_cell_length)
+    input_ent_text_padded = np.zeros([len(input_ent_text), max_cell_length], dtype=int)
+    for i, x in enumerate(input_ent_text):
+        input_ent_text_padded[i, :len(x)] = x
+
+    return [table_id,np.array(input_tok),np.array(input_tok_type),np.array(input_tok_pos),(np.array(input_tok_mask[0]),np.array(input_tok_mask[1])),len(input_tok), \
+                np.array(input_ent),input_ent_text_padded,input_ent_cell_length,np.array(input_ent_type),(np.array(input_ent_mask[0]),np.array(input_ent_mask[1])),len(input_ent), \
+                column_header_mask,column_entity_mask]
+
+    # headers, entities = input_table
+
+    # tokenized_headers = [config.tokenizer.encode(z, max_length=config.max_header_length, add_special_tokens=False) for z in headers]
+    # tokenized_headers_length = [len(z) for z in tokenized_headers]
+
+    # input_tokens, input_tokens_pos, input_tokens_type = [], [], []
+    # input_tokens += list(itertools.chain(*tokenized_headers))
+    # input_tokens_pos += list(itertools.chain(*[list(range(z)) for z in tokenized_headers_length]))
+    # input_tokens_type += [1] * sum(tokenized_headers_length)
+
+    # input_entities, input_entities_text, input_entities_type = [], [], []
+    # row_entity_map, column_entity_map = {}, {}
+
+    # # index refers to (row_index, column_index) of an entity in the table
+    # for e_i, (index, (entity_id, entity_text)) in enumerate(entities):
+    #     tokenized_entity_text = config.tokenizer.encode(entity_text, max_length=config.max_cell_length, add_special_tokens=False)
+        
+    #     input_entities.append(entity_id)
+    #     input_entities_text.append(tokenized_entity_text)
+    #     input_entities_type.append(4)
+
+    #     if index[0] not in row_entity_map:
+    #         row_entity_map[index[0]] = [e_i]
+    #     else:
+    #         row_entity_map[index[0]].append(e_i)
+
+    #     if index[1] not in column_entity_map:
+    #         column_entity_map[index[1]] = [e_i]
+    #     else:
+    #         column_entity_map[index[1]].append(e_i)
+    
+    # # create column entity mask
+    # column_entity_mask = np.zeros([len(headers), len(input_entities)], dtype=int)
+    
+    # for j in range(len(headers)):
+    #     for e_i in column_entity_map[j]:
+    #         column_entity_mask[j, e_i] = 1
+    
+    # # create column header mask
+    # start_i = 0
+    # header_span = {}
+    # column_header_mask = np.zeros([len(headers), len(input_tokens)], dtype=int)
+    
+    # for j in range(len(headers)):
+    #     header_span[j] = (start_i, start_i + tokenized_headers_length[j])
+    #     column_header_mask[j, header_span[j][0]:header_span[j][1]] = 1
+    #     start_i += tokenized_headers_length[j]
+    
+    # # create input mask
+    # tok_tok_mask = np.ones([len(input_tok), len(input_tok)], dtype=int)
+    # meta_ent_mask = np.ones([tokenized_meta_length, len(input_ent)], dtype=int)
+    # header_ent_mask = np.zeros([sum(tokenized_headers_length), len(input_ent)], dtype=int)
+    
+    # for e_i, (index, _) in enumerate(entities):
+    #     header_ent_mask[header_span[index[1]][0]:header_span[index[1]][1], e_i] = 1
+    # ent_header_mask = np.transpose(header_ent_mask)
+
+    # input_tok_mask = [tok_tok_mask, np.concatenate([meta_ent_mask, header_ent_mask], axis=0)]
+    # ent_meta_mask = np.ones([len(input_ent), tokenized_meta_length], dtype=int)
+
+    # return np.array(input_tokens), np.array(input_tokens_type), np.array(input_tokens_pos), len(input_tokens), np.array(input_entities), input_entities_text_padded, input_entities_cell_length, len(input_entities)
 
 
-class TurlWikiDataset(Dataset):
-    def __init__(self, data_dir: str, entity_vocab: dict, split: str, tokenizer=None, max_cells: int = 100, max_input_tokens: int = 350, max_input_entities: int = 150, max_header_length: int = 10, max_cell_length: int = 10, force_new: bool = False):
+class TurlWikiTableDataset(Dataset):
+    def __init__(self, data_dir: str, entity_vocab: dict, split: str, tokenizer=None, max_columns: int = 10, max_cells: int = 100, max_input_tokens: int = 350, max_input_entities: int = 150, max_header_length: int = 10, max_cell_length: int = 10, force_new: bool = False):
         self.data_dir = data_dir
         self.split = split
         self.entity_vocab = entity_vocab
         self.entity_id_map = {entity_vocab[x]["wiki_id"]: x for x in self.entity_vocab} # A dictionary that maps entity ID in Wikipedia to entity ID in TURL.
         self.tokenizer = tokenizer
+        self.max_columns = max_columns
         self.max_cells = max_cells
         self.max_input_tokens = max_input_tokens
         self.max_input_entities = max_input_entities
@@ -232,32 +384,42 @@ class TurlWikiDataset(Dataset):
             for line in tqdm(f):
                 num_orig_tables += 1
                 table = json.loads(line.strip())
+                
+                table_id = table.get("_id","")
+                pg_title = table.get("pgTitle", "").lower()
+                pg_entity = table.get("pgId", -1)
+                
+                if pg_entity != -1:
+                try:
+                    pg_entity = self.entity_id_map[pg_entity]
+                except:
+                    pg_entity = -1
+
+                sec_title = table.get("sectionTitle", "").lower()
+                caption = table.get("tableCaption", "").lower()
                 headers = table.get("processed_tableHeaders", []) 
                 rows = table.get("tableData", [])
                 entity_columns = table.get("entityColumn", []) # Index of entity columns, e.g., [0, 1, 2]
-                entity_cells = np.array(table.get("entityCell",[[]])) # Whether a cell is an entity cell, e.g., [[1, 1, 1, 0, 0], ...] 
+                entity_cells = np.array(table.get("entityCell",[[]])) # Whether a cell is an entity cell, e.g., [[1, 1, 1, 0, 0], ...]
+                headers = [headers[i] for i in entity_columns] # Headers for entity columns
 
-                # Headers for entity columns
-                headers = [[i, headers[i]] for i in entity_columns]
-
-                # Collect entities and entity mentions
+                # Collect entities (entity ids in TURL and entity mentions)
                 num_rows = len(rows)
                 num_cols = len(rows[0])
-                entities, entities_text = [], []
+                entities = []
                 split = [0]
                 tmp_entity_num = 0
 
                 for i in range(num_rows):
-                    tmp_entities, tmp_entities_text = [], []
+                    tmp_entities = []
 
-                    for j in range(num_cols):
+                    for j in range(min(num_cols, self.max_columns)):
                         if j in entity_columns:
                             if entity_cells[i][j] == 1:
                                 try:
-                                    entity = self.entity_id_map[rows[i][j]["surfaceLinks"][0]["target"]["id"]]
-                                    entity_cells[i][j] = entity
-                                    tmp_entities.append([(i,j), entity])
-                                    tmp_entities_text.append(rows[i][j]["text"])
+                                    entity_id = self.entity_id_map[rows[i][j]["surfaceLinks"][0]["target"]["id"]]
+                                    entity_text = rows[i][j]["text"]
+                                    tmp_entities.append([(i, j), (entity_id, entity_text)])
                                 except:
                                     entity_cells[i][j] = 0
                         else:
@@ -265,9 +427,8 @@ class TurlWikiDataset(Dataset):
                     
                     if len(tmp_entities) == 0: continue
 
-                    for (index, entity), entity_text in zip(tmp_entities, tmp_entities_text):
+                    for index, entity in tmp_entities:
                         entities.append([index, entity])
-                        entities_text.append(entity_text)
                         tmp_entity_num += 1
 
                     if tmp_entity_num >= self.max_cells:
@@ -283,23 +444,27 @@ class TurlWikiDataset(Dataset):
                 
                 for i in range(len(split) - 1):
                     actual_tables.append([
+                        table_id,
+                        pg_title,
+                        pg_entity,
+                        sec_title,
+                        caption,
                         headers,
-                        entities[split[i]:split[i+1]],
-                        entities_text[split[i]:split[i+1]]
-                    ])
+                        entities[split[i]:split[i+1]]])
         
         num_actual_tables = len(actual_tables)
         print("=" * 50)
         print("Number of original tables: ", num_orig_tables)
         print("Number of actual tables: ", num_actual_tables)
         print(actual_tables[0])
+        print("-" * 30)
+        print(actual_tables[2])
         print("=" * 50)
 
         pool = Pool(processes=4)
         processed_data = list(tqdm(
             pool.imap(
-                partial(process_single_hybrid_table, config=self), actual_tables, chunksize=2000
-                ), total=len(actual_tables)
+                partial(process_single_table_for_columns, config=self), actual_tables, chunksize=2000), total=len(actual_tables)
             ))
         pool.close()
 
@@ -346,5 +511,5 @@ if __name__ == "__main__":
 
     entity_vocab = load_entity_vocab(data_dir, ignore_bad_title=True, min_ent_count=min_ent_count)
 
-    test_data_loader = TurlWikiDataset(data_dir, entity_vocab, split="test")
+    test_data_loader = TurlWikiTableDataset(data_dir, entity_vocab, split="test")
     print(len(test_data_loader))
