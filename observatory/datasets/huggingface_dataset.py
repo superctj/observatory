51c6d275a0a082ec7dc2ecea6b77805da0f37b3d
AnthonySun1300 <123266336+AnthonySun1300@users.noreply.github.com>
2023-08-24 13:28:54 -0400

    Table preprocessing

--- observatory/datasets/huggingface_dataset.py
+++ observatory/datasets/huggingface_dataset.py
@@ -1,23 +1,73 @@
 from observatory.common_util.column_based_truncate import (
     max_rows,
     table2colList,
-    max_rows,
 )
 import pandas as pd
 from observatory.models.hugging_face_column_embeddings import column_based_process_table
 from observatory.models.hugging_face_cell_embeddings import cell_based_process_table
-
+import random
+from torch.utils.data import Dataset, DataLoader
+import torch
 from pandas import DataFrame
 
+from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
 
-def chunk_tables(tables, max_col, model_name, max_length, tokenizer, max_row=None):
+def n_gram_overlap(text, row_text, n=2):
+    vectorizer = CountVectorizer(ngram_range=(n, n))
+    counts = vectorizer.fit_transform([text, row_text])
+    overlap = sum((counts.toarray()[0] & counts.toarray()[1]) > 0)
+    return overlap
+
+def get_most_similar_rows(df, text_to_compare, n=2, k=2):
+    overlaps = []
+    for _, row in df.iterrows():
+        row_text = ' '.join(row.astype(str))
+        overlap = n_gram_overlap(text_to_compare, row_text, n=n)
+        overlaps.append((overlap, row))
+    
+    # Sort by overlap and take the top k rows
+    sorted_rows = sorted(overlaps, key=lambda x: x[0], reverse=True)[:k]
+    most_similar_rows = pd.DataFrame([row for _, row in sorted_rows])
+    
+    return most_similar_rows
+
+def get_most_similar_cols(df, text_to_compare, n=2, k=2):
+    overlaps = {}
+    # Compute overlap for each column
+    for col in df.columns:
+        overlaps[col] = n_gram_overlap(text_to_compare, df[col], n=n)
+        
+    # Sort columns by overlap
+    sorted_columns = sorted(overlaps, key=overlaps.get, reverse=True)[:k]
+    
+    # Keep the most similar k columns
+    most_similar_cols_df = df[sorted_columns]
+    
+    return most_similar_cols_df
+
+def random_sample_rows(df, k):
+    return df.sample(n=k)
+
+def random_sample_cols(df, k):
+    random_cols = random.sample(list(df.columns), k)
+    return df[random_cols]
+
+def sample_top_k_rows(df, k):
+    return df.head(k)
+
+def sample_top_k_cols(df, k):
+    return df.iloc[:, :k]
+
+def chunk_tables(tables,  model_name, max_length, tokenizer, max_col=None, max_row=None, max_token_per_cell=None):
     chunked_tables = {}
-
+    
     # Iterate over each table
     for table_index, table in enumerate(tables):
         # Convert table to pandas DataFrame for easier manipulation
         df = DataFrame(table)
-
+        if not max_col:
+            max_col = df.shape[1]
         chunked_tables[table_index] = {}
         # Split each table into chunks based on max_col
         start_col = 0
@@ -30,8 +80,10 @@ def chunk_tables(tables, max_col, model_name, max_length, tokenizer, max_row=Non
             while start_row < chunk.shape[0]:
                 optimal_rows = 0
                 approxiamte_optimal_rows = max_length // chunk.shape[1]
-                if max_row:
-                    approxiamte_optimal_rows = max_row
+                if max_token_per_cell:
+                    approxiamte_optimal_rows = max_length // (chunk.shape[1] * max_token_per_cell)
+                    if max_row:
+                        approxiamte_optimal_rows = min(max_row,approxiamte_optimal_rows)
                 end_row = min(start_row + approxiamte_optimal_rows, chunk.shape[0])
                 sub_chunk = chunk.iloc[start_row:end_row, :]
                 cols = table2colList(sub_chunk)
@@ -39,6 +91,8 @@ def chunk_tables(tables, max_col, model_name, max_length, tokenizer, max_row=Non
 
                 # Ensure optimal_rows is at least 1 to prevent infinite loops
                 if optimal_rows == 0:
+                    print("Column headers too Many, please set a vaild max_col or reduce teh current max_col!")
+                    print("You may also try to set a vaild max_token_per_cell or reduce teh current max_token_per_cell")
                     start_row = start_row + 1
                     continue
 
@@ -65,9 +119,7 @@ def chunk_tables(tables, max_col, model_name, max_length, tokenizer, max_row=Non
     return chunked_list
 
 
-from torch.utils.data import Dataset, DataLoader
-from pandas import DataFrame
-import torch
+
 
 
 class TableColumnDataset(Dataset):
