9bb9e96de764b5d5ff2628ae9fdf5914ced63f26
superctj <congtj@umich.edu>
2024-03-22 13:24:38 -0400

    review code and enhance docstrings

--- observatory/properties/Sample_Fidelity/evaluate_Sample_Fidelity.py
+++ observatory/properties/Sample_Fidelity/evaluate_Sample_Fidelity.py
@@ -1,82 +1,86 @@
-import os
 import argparse
+import itertools
+import math
+import os
+import random
+
 import pandas as pd
 import torch
 import torch.nn as nn
+
+from observatory.common_util.mcv import compute_mcv
+from observatory.common_util.truncate import truncate_index
+from observatory.models.hugging_face_column_embeddings import (
+    get_hugging_face_column_embeddings_batched,
+)
 from observatory.models.huggingface_models import (
     load_transformers_model,
     load_transformers_tokenizer_and_max_length,
 )
-from observatory.common_util.truncate import truncate_index
-from observatory.common_util.mcv import compute_mcv
-import random
-import math
-import itertools
-from observatory.models.hugging_face_column_embeddings import (
-    get_hugging_face_column_embeddings_batched
-)
 
 
-def get_subsets(n: int, m: int, portion: float) -> list[list[int]]:
-    """Get m distinct subsets of size n * portion
-        from the set {0, 1, ..., n - 1}.
-    If the number of possible subsets is small,
-        generate all combinations and randomly select from them.
-    If the number of possible subsets is large,
-        use random sampling to generate distinct subsets.
+def get_subsets(n: int, m: int, frac: float) -> list[list[int]]:
+    """Get up to m distinct subsets of size n * frac from the set {0, 1, ...,
+    n - 1}.
+
+    If the total number of unique subsets is small (smaller than 10 * m),
+    generate all combinations and randomly select from them; else, use random
+    sampling to generate distinct subsets.
 
     Args:
-        n: The size of the set.
-        m: The number of subsets to generate.
-        portion: The portion of the set to use in each subset.
+        n: The size of the whole set.
+        m: The number of subsets to return.
+        frac: Sampling fraction.
 
     Returns:
-        list: A list of m distinct subsets of size n * portion.
-        """
-    portion_size = int(n * portion)
-    max_possible_tables = math.comb(n, portion_size)
+        A list of up to m distinct subsets of size n * frac.
+    """
 
-    if max_possible_tables <= 10 * m:
-        # If the number of combinations is small, generate all combinations 
-        # and randomly select from them
-        all_subsets = list(itertools.combinations(range(n), portion_size))
+    subset_size = int(n * frac)
+    num_uniq_subsets = math.comb(n, subset_size)
+
+    if num_uniq_subsets <= 10 * m:
+        all_subsets = list(itertools.combinations(range(n), subset_size))
         random.shuffle(all_subsets)
         return [list(subset) for subset in all_subsets[:m]]
     else:
-        # If the number of combinations is large, use random sampling 
-        # to generate distinct subsets
         subsets = set()
-        while len(subsets) < min(m, max_possible_tables):
-            new_subset = tuple(sorted(random.sample(range(n), portion_size)))
+
+        while len(subsets) < min(m, num_uniq_subsets):
+            new_subset = tuple(sorted(random.sample(range(n), subset_size)))
             subsets.add(new_subset)
+
         return [list(subset) for subset in subsets]
 
 
 def sample_df(
-        df: pd.DataFrame, m: int, portion: float
+    df: pd.DataFrame, m: int, frac: float
 ) -> tuple[list[pd.DataFrame], list[list[int]]]:
-    """Sample the rows of a dataframe by at most m samples.
+    """Get up to m unqiue samples of a table at a sampling fraction.
 
     Args:
         df: A dataframe to sample.
-        m: The number of unique samples to generate excluding the original
-            sequence.
+        m: The number of unique samples to generate.
+        frac: The sampling fraction.
 
     Returns:
-        dfs: A list of the orginal dataframes (at the begining, i.e., dfs[0])
-            and at most m row-wise sampled dataframes.
-        uniq_permuts: A list of the original permutation
-            (at the begining, i.e., uniq_permuts[0])
-            and the permutations used to shuffle the rows.
+        dfs: A list of at most m row-wise sampled dataframes plus the orginal
+          dataframes at the beginning, i.e., dfs[0].
+        uniq_permuts: A list of row identifiers of each sample plus the
+          original row order at the begining, i.e., uniq_permuts[0]).
     """
-    subsets = get_subsets(len(df), m, portion)
+
+    subsets = get_subsets(len(df), m, frac)
     uniq_permuts = [list(range(len(df)))] + subsets
     dfs = [df]
+
     for subset in subsets:
         dfs.append(df.iloc[subset])
+
     return dfs, uniq_permuts
 
 
+# TODO: This is a duplicate function that can be extracted out
 def analyze_embeddings(
     all_embeddings: list[list[torch.FloatTensor]],
 ) -> tuple[list[float], list[float], float, float]:
@@ -90,53 +94,59 @@ def analyze_embeddings(
             contains column embeddings of a table induced by a permutation.
 
     Returns:
-        avg_cosine_similarities: A list of the average pairwise cosine
-            similarities. E.g., avg_cosine_similarities[0] is the average of
-            pairwise cosine similarities of the embedding population induced by
-            the first column in the original table.
-        mcvs: A list of MCV values. E.g., mcvs[0] is the MCV of the embedding
-            population induced by the first column in the original table.
+        colwise_avg_cosine_similarities: Average pairwise cosine similarities
+            per column. E.g., colwise_avg_cosine_similarities[0] is the
+            average of pairwise cosine similarities of the embedding population
+            induced by the first column in the original table.
+        colwise_mcvs: MCV values per column. E.g., colwise_mcvs[0] is the MCV
+            value of the embedding population induced by the first column in the
+            original table.
         table_avg_cosine_similarity: The cosine similarity averaged over
-            columns, i.e., the average of `avg_cosine_similarities`.
+            columns, i.e., the average of `colwise_avg_cosine_similarities`.
         table_avg_mcv: The MCV value averaged over columns, i.e., the average
-            of `mcvs`.
+            of `colwise_mcvs`.
     """
 
-    avg_cosine_similarities = []
-    mcvs = []
+    colwise_avg_cosine_similarities = []
+    colwise_mcvs = []
 
     for i in range(len(all_embeddings[0])):
         column_cosine_similarities = []
-        column_embeddings = []
 
-        for j in range(len(all_embeddings)):
-            column_embeddings.append(all_embeddings[j][i])
-
+        # iterate over permutations
         for j in range(1, len(all_embeddings)):
-            truncated_embedding = all_embeddings[0][i]
+            # embedding of a column inferred from the original table
+            col_embedding = all_embeddings[0][i]
+            # embedding of the same column inferred from a shuffled table
             shuffled_embedding = all_embeddings[j][i]
 
             cosine_similarity = nn.functional.cosine_similarity(
-                truncated_embedding, shuffled_embedding, dim=0
+                col_embedding, shuffled_embedding, dim=0
             )
             column_cosine_similarities.append(cosine_similarity.item())
 
         avg_cosine_similarity = sum(column_cosine_similarities) / len(
             column_cosine_similarities
         )
+        colwise_avg_cosine_similarities.append(avg_cosine_similarity)
+
+        # compute MCV
+        column_embeddings = []
+
+        for j in range(len(all_embeddings)):
+            column_embeddings.append(all_embeddings[j][i])
+
         mcv = compute_mcv(torch.stack(column_embeddings))
+        colwise_mcvs.append(mcv)
 
-        avg_cosine_similarities.append(avg_cosine_similarity)
-        mcvs.append(mcv)
-
-    table_avg_cosine_similarity = sum(avg_cosine_similarities) / len(
-        avg_cosine_similarities
+    table_avg_cosine_similarity = sum(colwise_avg_cosine_similarities) / len(
+        colwise_avg_cosine_similarities
     )
-    table_avg_mcv = sum(mcvs) / len(mcvs)
+    table_avg_mcv = sum(colwise_mcvs) / len(colwise_mcvs)
 
     return (
-        avg_cosine_similarities,
-        mcvs,
+        colwise_avg_cosine_similarities,
+        colwise_mcvs,
         table_avg_cosine_similarity,
         table_avg_mcv,
     )
@@ -145,13 +155,12 @@ def analyze_embeddings(
 def process_table_wrapper(
     table_index: int,
     truncated_table: pd.DataFrame,
-    args: argparse.Namespace,
     model_name: str,
     model,
     tokenizer,
-    device: torch.device,
     max_length: int,
-    padding_token: str,
+    args: argparse.Namespace,
+    device: torch.device,
 ) -> None:
     """Processes a single table and saves the embeddings and results.
 
@@ -169,6 +178,7 @@ def process_table_wrapper(
     Returns:
         None (saves the embeddings and results to the specified directories).
     """
+
     save_directory_results = os.path.join(
         args.save_directory,
         "Sample_Fidelity",
@@ -188,22 +198,24 @@ def process_table_wrapper(
         os.makedirs(save_directory_embeddings)
     if not os.path.exists(save_directory_results):
         os.makedirs(save_directory_results)
-    sampled_tables, perms = sample_df(
+
+    sampled_tables, _ = sample_df(
         truncated_table, args.num_samples, args.sample_portion
     )
-
     all_embeddings = get_hugging_face_column_embeddings_batched(
         tables=sampled_tables,
         model_name=model_name,
         tokenizer=tokenizer,
         max_length=max_length,
         model=model,
-        batch_size=args.batch_size
+        batch_size=args.batch_size,
+        device=device,
     )
 
     save_file_path = os.path.join(
         save_directory_embeddings, f"table_{table_index}_embeddings.pt"
     )
+
     # If the file exists, load it and substitute the elements.
     if os.path.exists(save_file_path):
         existing_embeddings = torch.load(save_file_path)
@@ -213,9 +225,7 @@ def process_table_wrapper(
             existing_embeddings = all_embeddings
         else:
             # Substitute the elements
-            existing_embeddings[
-                : len(all_embeddings)
-            ] = all_embeddings
+            existing_embeddings[: len(all_embeddings)] = all_embeddings
 
         # Save the modified embeddings
         torch.save(existing_embeddings, save_file_path)
@@ -229,21 +239,26 @@ def process_table_wrapper(
         table_avg_cosine_similarity,
         table_avg_mcv,
     ) = analyze_embeddings(all_embeddings)
+
     results = {
         "avg_cosine_similarities": avg_cosine_similarities,
         "mcvs": mcvs,
         "table_avg_cosine_similarity": table_avg_cosine_similarity,
         "table_avg_mcv": table_avg_mcv,
     }
+
     print(f"Table {table_index}:")
     print("Average Cosine Similarities:", results["avg_cosine_similarities"])
     print("MCVs:", results["mcvs"])
-    print("Table Average Cosine Similarity:",
-          results["table_avg_cosine_similarity"])
+    print(
+        "Table Average Cosine Similarity:",
+        results["table_avg_cosine_similarity"],
+    )
     print("Table Average MCV:", results["table_avg_mcv"])
+
     torch.save(
-        results, os.path.join(save_directory_results,
-                              f"table_{table_index}_results.pt")
+        results,
+        os.path.join(save_directory_results, f"table_{table_index}_results.pt"),
     )
 
 
@@ -260,37 +275,37 @@ def process_and_save_embeddings(
     Returns:
         None (saves the embeddings and results to the specified directories).
     """
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = load_transformers_model(model_name, device)
+
     tokenizer, max_length = load_transformers_tokenizer_and_max_length(
         model_name
     )
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(device)
-    model = load_transformers_model(model_name, device)
-    model.eval()
-    padding_token = "<pad>" if model_name.startswith("t5") else "[PAD]"
 
     for table_index, table in enumerate(tables):
         if table_index < args.start_index:
             continue
+
         max_rows_fit = truncate_index(table, tokenizer, max_length, model_name)
         truncated_table = table.iloc[:max_rows_fit, :]
+
         process_table_wrapper(
             table_index,
             truncated_table,
-            args,
             model_name,
             model,
             tokenizer,
-            device,
             max_length,
-            padding_token,
+            args,
+            device,
         )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Process tables and save embeddings."
-        )
+    )
     parser.add_argument(
         "-r",
         "--read_directory",
@@ -344,21 +359,19 @@ if __name__ == "__main__":
     table_files = [
         f for f in os.listdir(args.read_directory) if f.endswith(".csv")
     ]
-    normal_tables = []
-    for file in table_files:
+    all_tables = []
+
+    for f in table_files:
         table = pd.read_csv(
-            f"{args.read_directory}/{file}",
-            keep_default_na=False
+            os.path.join(args.read_directory, f), keep_default_na=False
         )
-        normal_tables.append(table)
+        all_tables.append(table)
 
     if args.model_name == "":
-        model_names = ["bert-base-uncased", "roberta-base", "t5-base"]
+        all_model_names = ["bert-base-uncased", "roberta-base", "t5-base"]
     else:
-        model_names = [args.model_name]
-    print()
-    print("Evaluate row shuffle for: ", model_names)
-    print()
+        all_model_names = [args.model_name]
 
-    for model_name in model_names:
-        process_and_save_embeddings(model_name, normal_tables, args)
+    for model_name in all_model_names:
+        print(f"\nEvaluate sample fidelity for: {model_name}\n")
+        process_and_save_embeddings(model_name, all_tables, args)
